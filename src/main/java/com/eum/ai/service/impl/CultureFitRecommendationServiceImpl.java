40035c21e08eb9f5ed078723b5c305ce147c921e
dmsskgus@naver.com <dmsskgus@naver.com>
2025-05-07 12:37:18 +0900

    refactor : ai buildPrompt메소드 별도 클래스로 분리

--- src/main/java/com/eum/ai/service/impl/CultureFitRecommendationServiceImpl.java
+++ src/main/java/com/eum/ai/service/impl/CultureFitRecommendationServiceImpl.java
@@ -22,6 +22,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.eum.ai.util.BuildPrompt.buildPrompt;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -103,21 +105,4 @@ public class CultureFitRecommendationServiceImpl implements CultureFitRecommenda
                 });
     }
 
-    private String buildPrompt (CultureFitRequest cultureFitRequest) {
-        return "다음은 사용자의 컬쳐핏 성향 질문에 대한 응답입니다. 아래 JSON 응답을 바탕으로 아래 7가지 유형 중 가장 어울리는 1개만 선택해 주세요. \n\n"+
-                "1. 당신은 어떤 협업 방식을 선호하나요? " + cultureFitRequest.collaborationStyle() + "\n" +
-                "2. 마감일이 다가올 때 당신의 작업 스타일은 어떤가요?" + cultureFitRequest.deadlineAttitude() + "\n" +
-                "3. 프로젝트 중 문제가 생기면 어떤 방식으로 해결하려 하나요?" + cultureFitRequest.problemSolvingApproach()+ "\n" +
-                "4. 팀 내 소통에서 당신의 성향은?" + cultureFitRequest.communicationStyle() + "\n" +
-                "5. 팀원과 의견이 다를 때 어떤 편인가요?" + cultureFitRequest.conflictResolution() + "\n" +
-                "6. 당신은 어떤 방식의 업무 분배를 선호하나요?" + cultureFitRequest.tackDistribution() + "\n" +
-                "가능한 컬쳐핏 유형:\n" +
-                "자율형 (자기 주도 및 개인 중심), 계획형 (체계적·신중한 스타일), 소통 협업형 (협력·의사소통 중시), 실용 협업형 (효율성과 실행력 중심), 조화 중시형 (관계 중심), 지시 기반형 (명확한 체계 선호)\n\n" +
-                "컬쳐핏 유형의 응답은 아래 중 하나를 골라 답해주세요.\n"+
-                "자율형 : AUTONOMOUS, 계획형 : PLANNER, 소통 협업형 : COMMUNICATIVE, 실용 협업형 : PRACTICAL, 조화 중시형 : HARMONY, 지시 기반형 :DIRECTIVE\n"+
-                "형식은 다음과 같이 응답해주세요:\n" +
-                "{\n" +
-                "   \"cultureFitType\": \"...\"\n" +
-                "}";
-    }
 }
