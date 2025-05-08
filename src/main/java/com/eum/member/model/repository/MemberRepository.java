ba555c4bd286e50cd9ef97d2e5ca506e753e8f35
KimS1Yong <sdragon0928@naver.com>
2025-05-08 12:16:49 +0900

    refactor: ERD 설계에 맞게 수정

--- src/main/java/com/eum/member/model/repository/MemberRepository.java
+++ src/main/java/com/eum/member/model/repository/MemberRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findMemberByAuthIdAndProvider(String authId, String provider);
+
+    Optional<Member> findByPublicId(UUID publicId);
 }
