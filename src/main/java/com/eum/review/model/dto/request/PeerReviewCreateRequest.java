f499a5e49c97a705acf61023fa79d80a05c2c9eb
nan0silver <silvernh1220@gmail.com>
2025-05-10 23:39:23 +0900

    refector : PeerReviewCreateRequest revieweeId를 revieweePublicId (UUID)로 수정

--- src/main/java/com/eum/review/model/dto/request/PeerReviewCreateRequest.java
+++ src/main/java/com/eum/review/model/dto/request/PeerReviewCreateRequest.java
@@ -3,15 +3,17 @@ package com.eum.review.model.dto.request;
 import com.eum.post.model.entity.Post;
 import com.eum.review.model.entity.PeerReview;
 
+import java.util.UUID;
+
 public record PeerReviewCreateRequest(
         Long postId,
-        Long revieweeMemberId,
+        UUID revieweePublicId,
         Integer collaborationScore,
         Integer technicalScore,
         Integer workAgainScore,
         String reviewComment
 ) {
-    public PeerReview toEntity(Long reviewerMemberId, Post post){
+    public PeerReview toEntity(Long reviewerMemberId, Long revieweeMemberId,Post post){
         return PeerReview.of(
                 reviewerMemberId,
                 revieweeMemberId,
