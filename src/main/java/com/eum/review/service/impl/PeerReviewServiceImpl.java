f3181ca546c34ed6f8662bdd07471f7cac23837d
nan0silver <silvernh1220@gmail.com>
2025-05-10 15:22:14 +0900

    feat : post 완료 후 팀원 피어리뷰 작성 가능하도록 수정

--- src/main/java/com/eum/review/service/impl/PeerReviewServiceImpl.java
+++ src/main/java/com/eum/review/service/impl/PeerReviewServiceImpl.java
@@ -4,6 +4,7 @@ import com.eum.global.exception.CustomException;
 import com.eum.global.exception.ErrorCode;
 import com.eum.post.model.entity.Post;
 import com.eum.post.model.entity.enumerated.Status;
+import com.eum.post.model.repository.PostMemberRepository;
 import com.eum.post.model.repository.PostRepository;
 import com.eum.review.model.dto.request.PeerReviewCreateRequest;
 import com.eum.review.model.dto.response.PeerReviewResponse;
@@ -24,23 +25,34 @@ import java.util.stream.Collectors;
 public class PeerReviewServiceImpl implements PeerReviewService {
     private final PeerReviewRepository peerReviewRepository;
     private final PostRepository postRepository;
+    private final PostMemberRepository postMemberRepository;
 
     @Transactional
     @Override
     public PeerReviewResponse createReview(PeerReviewCreateRequest request, Long reviewerUserId) {
+        // 1. 자기 자신에 대한 리뷰가 아닌지 확인
         if (reviewerUserId.equals(request.revieweeMemberId())){
             throw new CustomException(ErrorCode.SELF_REVIEW_NOT_ALLOWED);
         }
 
+        // 2. 게시글이 있는지 확인
         Post post = postRepository.findById(request.postId())
                 .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
 
+        // 3. 게시글이 완료상태인지 확인
         if (!post.getStatus().equals(Status.COMPLETED)) {
             throw new CustomException(ErrorCode.POST_NOT_COMPLETE);
         }
 
-        // TODO: 리뷰 작성자와 대상자가 프로젝트의 팀원인지 검증하는 로직 추가 필요
-        // 팀원 관계 테이블 개발 완료 후 코드 추가 예정
+        // 4. 리뷰어가 팀의 멤버인지 확인
+        if (!postMemberRepository.existsByPostIdAndMemberId(request.postId(), reviewerUserId)){
+            throw new CustomException(ErrorCode.POST_ACCESS_DENIED, "리뷰어는 해당 팀의 멤버여야 합니다.");
+        }
+
+        // 5. 리뷰 대상이 팀의 멤버인지 확인
+        if (!postMemberRepository.existsByPostIdAndMemberId(request.postId(), request.revieweeMemberId())) {
+            throw new CustomException(ErrorCode.REVIEW_TARGET_NOT_TEAM_MEMBER);
+        }
 
         PeerReview peerReview = request.toEntity(reviewerUserId, post);
         PeerReview savedReview = peerReviewRepository.save(peerReview);
